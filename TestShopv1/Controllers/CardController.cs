3187ad7f69dfe42c7f4db50c8d720ea6ecb883d4
Dennis Olcay <ITN262232@qualifizierung.at>
2023-05-17 14:59:14 +0200

    finish a order

--- TestShopv1/Controllers/CardController.cs
+++ TestShopv1/Controllers/CardController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -15,6 +16,8 @@ namespace TestShopv1.Controllers
     {
         [BindProperty]
         public CardVM CardVM { get; set; }
+        public static OrderLine OrderLineTemp { get; set; }
+        public static decimal? TotalPrice { get; set; }
         private readonly MyContext _db;
         public CardController(MyContext myContext)
         {
@@ -74,23 +77,32 @@ namespace TestShopv1.Controllers
 
             decimal tax02 = 0.0m;
             decimal tax01 = 0.0m;
+            decimal tax = 0.0m;
             decimal unitgesamt = 0.0m;
+            decimal unitgesamtNet = 0.0m;
             var catL = _db.Categories.ToList();
             foreach (var item in cards)
             {
-                unitgesamt += (item.Product.UnitPriceNetto * item.Amount);
-                TempData["Netto"] = unitgesamt;
+                unitgesamt = (item.Product.UnitPriceNetto * item.Amount);
+                unitgesamtNet += unitgesamt;
                 if (catL.FirstOrDefault(x => x.Id == item.Product.CategoryId).TaxRate == 0.20m)
                 {
-                    tax02 += (decimal)(unitgesamt * item.Product.Category.TaxRate);
+                    tax02 = (decimal)((item.Product.UnitPriceNetto * item.Amount) * item.Product.Category.TaxRate);
+                    tax += tax02;
                     CardVM.OrderTotal += (unitgesamt + tax02);
-                    TempData["Steuer"] = tax02;
                 }
-                else if (catL.FirstOrDefault(x => x.Id == item.Product.CategoryId).TaxRate == 0.10m)
-                {
-                    tax01 += (decimal)(unitgesamt * item.Product.Category.TaxRate);
-                    CardVM.OrderTotal += unitgesamt + tax01;
-                }
+                #region 10%
+
+                //else if (catL.FirstOrDefault(x => x.Id == item.Product.CategoryId).TaxRate == 0.10m)
+                //{
+                //    tax01 = (decimal)((item.Product.UnitPriceNetto * item.Amount) * item.Product.Category.TaxRate);
+                //    CardVM.OrderTotal += unitgesamt + tax01;
+                //}
+
+                #endregion
+
+                TempData["Steuer"] = tax.ToString("C", CultureInfo.CreateSpecificCulture("de-DE"));
+                TempData["Netto"] = unitgesamtNet.ToString("C", CultureInfo.CreateSpecificCulture("de-DE"));
                 CardVM.Order.TotalPriceBrutto = CardVM.OrderTotal;
                 CardVM.Order.BruttoPreis = (decimal)CardVM.Order.TotalPriceBrutto;
             }
@@ -127,12 +139,14 @@ namespace TestShopv1.Controllers
         [ActionName("Summary")]
         public IActionResult SummaryPost()
         {
+            OrderLineTemp = null;
             var claimsidentity = (ClaimsIdentity)User.Identity;
             var userId = claimsidentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             int userID = int.Parse(userId);
             CardVM.CardList = _db.ShoppingCards.Include(x => x.Product).Include(x => x.Category).Include(x => x.Customer).Where(s => s.CustomerId == userID).ToList();
-            ////NettoBruttoTotal_Rechner(CardVM.CardList);
-
+ 
+            TotalPrice = NettoBruttoTotal_Rechner(CardVM.CardList);
+    
             CardVM.Order.OrderedOn = DateTime.Now;
             CardVM.Order.CustomerId = userID;
 
@@ -148,12 +162,12 @@ namespace TestShopv1.Controllers
                 {
                     ProductId = item.Product.Id,
                     OrderId = CardVM.Order.Id,
-                    TotalPriceBrutto = NettoBruttoTotal_Rechner(CardVM.CardList),
+                    TotalPriceBrutto = ((item.Product.UnitPriceNetto * item.Amount) * (1.0m + _db.Categories.FirstOrDefault(x => x.Id == item.Product.CategoryId).TaxRate)),
                     Quantity = item.Amount,
-                    TaxRate = item.Product.Category.TaxRate,
+                    TaxRate = _db.Categories.FirstOrDefault(x => x.Id ==  item.Product.CategoryId).TaxRate ,
                     ShoppingCardId = item.Id
                 };
-                TempData["Order"] = orderLine;
+                       OrderLineTemp = orderLine;
                 _db.OrderLines.Add(orderLine);
                 _db.SaveChanges();
             }
@@ -163,11 +177,12 @@ namespace TestShopv1.Controllers
 
         public IActionResult OrderConfirmation()
         {
-            //OrderLine orderLine = new()
-            //{
-            //    Id = 
-            //};
-            return View();
+
+
+            OrderLine orderLine = OrderLineTemp;
+            orderLine.TotalPriceBrutto = TotalPrice;
+
+            return View(orderLine);
         }
     }
 }
